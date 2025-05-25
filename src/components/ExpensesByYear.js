8ac98cd5e1fbc3b641aace285dc346166d8fe55c
will chapman <williamchapman53@gmail.com>
2025-05-25 10:08:18 -0400

    edited style.css and category for actual months

--- src/components/ExpensesByYear.js
+++ src/components/ExpensesByYear.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
+
 const ExpensesByYear = () => {
   const [year, setYear] = useState('');
   const [results, setResults] = useState([]);
